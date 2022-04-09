31ec33c5b100e1fdbe2ebd8a6c0ce5cd1d63ad17
Shean92 <sheanthebond92@gmail.com>
2022-04-08 23:13:38 -0700

    adding files and fixing stuff

--- UnityTools/Base_Builder_Tool/Controller.cs
+++ UnityTools/Base_Builder_Tool/Controller.cs
@@ -4,91 +4,24 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-
-    private int buildSelect;
-    private int totalStructures;
-    public float wheelThreshold;
-    public float rotateSpeed;
-    public float transparency;
-    public List<string> resources;
-
-    [System.Serializable]
-    public class Structure
-    {
-        public GameObject structureObject;
-        public int cost;
-
-        public Structure(GameObject structureObject)
-        {
-            this.structureObject = structureObject;
-        }
-    };
-
-    public GameObject currentObject;
-    public List<Structure> structureList;
+    public bool BuildingModeEnabled = false;
 
     private void Start()
     {
-        totalStructures = structureList.Count - 1;
-        buildSelect = 0;
-        MakePhantom();
+        this.GetComponent<BuildingMode>().enabled = BuildingModeEnabled;
     }
-    private void Update()
+    void Update()
     {
-        if (Input.GetAxisRaw("Mouse ScrollWheel") > wheelThreshold)
+        if (BuildingModeEnabled == false && this.GetComponent<BuildingMode>().currentObject)
         {
-            DestroyPhantom();
-            buildSelect++;
-            MaintainBuildSelect();
-            MakePhantom();
+            Destroy(this.GetComponent<BuildingMode>().currentObject);
         }
-        else if (Input.GetAxisRaw("Mouse ScrollWheel") < -wheelThreshold)
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            DestroyPhantom();
-            buildSelect--;
-            MaintainBuildSelect();
-            MakePhantom();
+            BuildingModeEnabled = !BuildingModeEnabled;
+            Destroy(this.GetComponent<BuildingMode>().currentObject);
+            this.GetComponent<BuildingMode>().enabled = BuildingModeEnabled;
         }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 mousePos = Input.mousePosition;
-            mousePos.z = 0.0f;
-            Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
-            objectPos.z = 0.0f;
-            Quaternion currentRotation = currentObject.GetComponent<Transform>().rotation;
-            PlaceStructure(objectPos, currentRotation);
-        }
-
-        if (Input.GetMouseButton(1))
-        {
-            currentObject.GetComponent<Transform>().Rotate(Vector3.back * rotateSpeed);
-        }
-    }
-
-    void MaintainBuildSelect()
-    {
-        if (buildSelect > totalStructures) buildSelect = 0;
-        else if (buildSelect < 0) buildSelect = totalStructures;
-    }
-
-    void PlaceStructure(Vector3 objectPos, Quaternion currentRotation)
-    {
-        Instantiate(structureList[buildSelect].structureObject, objectPos, currentRotation);
-    }
-
-    void MakePhantom()
-    {
-        currentObject = (GameObject)GameObject.Instantiate(structureList[buildSelect].structureObject);
-        Color currentObjectTransparent = currentObject.GetComponent<SpriteRenderer>().color;
-        currentObjectTransparent.a = transparency;
-        currentObject.GetComponent<SpriteRenderer>().color = currentObjectTransparent;
-        currentObject.GetComponent<PhantomStructure>().enabled = true;
-    }
-
-    void DestroyPhantom()
-    {
-        Destroy(currentObject);
     }
 }
 
